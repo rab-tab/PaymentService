a9007ec3e74e345ba39531a8ace512fe10b69c4e
Rabia <sdetcode@gmail.com>
2023-02-18 18:17:56 +0530

    initial setup

---
+++ src/test/com/dailycodebuffer/PaymentService/PaymentServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package PaymentService.src.test.com.dailycodebuffer.PaymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
